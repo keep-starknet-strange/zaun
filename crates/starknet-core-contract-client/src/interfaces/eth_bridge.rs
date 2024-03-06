f2e1eb0f506585f3a46eed1c7d8207e3f01b08e6
Anshal Shukla <shukla.anshal85@gmail.com>
2024-03-06 23:27:36 +0530

    add: methods to eth bridge

--- crates/starknet-core-contract-client/src/interfaces/eth_bridge.rs
+++ crates/starknet-core-contract-client/src/interfaces/eth_bridge.rs
@@ -11,6 +11,10 @@ use crate::Error;
 abigen!(
     StarknetEthBridge,
     r#"[
+        function setMaxTotalBalance(uint256 maxTotalBalance_) external onlyGovernance
+        function setMaxDeposit(uint256 maxDeposit_) external onlyGovernance
+        function setL2TokenBridge(uint256 l2TokenBridge_) external onlyGovernance
+
         function deposit(uint256 amount, uint256 l2Recipient) public payable override
 
         function identify() external pure override returns (string memory)
@@ -19,6 +23,9 @@ abigen!(
 
 #[async_trait]
 pub trait StarknetEthBridgeTrait<M: Middleware> {
+    async fn set_max_total_balance(&self, max_total_balance: U256) -> Result<Option<TransactionReceipt>, Error<M>>;
+    async fn set_max_deposit(&self, max_deposit: U256) -> Result<Option<TransactionReceipt>, Error<M>>;
+    async fn set_l2_token_bridge(&self, l2_token_bridge: U256) -> Result<Option<TransactionReceipt>, Error<M>>;
     async fn deposit(&self, amount: U256, l2recipient: U256) -> Result<Option<TransactionReceipt>, Error<M>>;
     async fn identify(&self) -> Result<String, Error<M>>;
 }
@@ -28,6 +35,36 @@ impl<T, M: Middleware> StarknetEthBridgeTrait<M> for T
     where
         T: AsRef<StarknetEthBridge<M>> + Send + Sync,
 {
+    async fn set_max_total_balance(&self, max_total_balance: U256) -> Result<Option<TransactionReceipt>, Error<M>> {
+        self.as_ref()
+            .set_max_total_balance(max_total_balance)
+            .send()
+            .await
+            .map_err(Into::<ContractError<M>>::into)?
+            .await
+            .map_err(Into::into)
+    }
+
+    async fn set_max_deposit(&self, max_deposit: U256) -> Result<Option<TransactionReceipt>, Error<M>> {
+        self.as_ref()
+            .set_max_deposit(max_deposit)
+            .send()
+            .await
+            .map_err(Into::<ContractError<M>>::into)?
+            .await
+            .map_err(Into::into)
+    }
+
+    async fn set_l2_token_bridge(&self, l2_token_bridge: U256) -> Result<Option<TransactionReceipt>, Error<M>> {
+        self.as_ref()
+            .set_l2_token_bridge(l2_token_bridge)
+            .send()
+            .await
+            .map_err(Into::<ContractError<M>>::into)?
+            .await
+            .map_err(Into::into)
+    }
+
     async fn deposit(&self, amount: U256, l2recipient: U256) -> Result<Option<TransactionReceipt>, Error<M>> {
         self.as_ref()
             .deposit(amount, l2recipient)
