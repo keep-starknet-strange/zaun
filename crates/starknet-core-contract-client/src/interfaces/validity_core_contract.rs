a37c47dbca1a64d5d697d7fcdf18223d2cd7f63d
anonXcoder <anonxcoder@tutanota.com>
2024-03-29 00:49:31 +0530

    interfaces alloy migration wip

--- crates/starknet-core-contract-client/src/interfaces/validity_core_contract.rs
+++ crates/starknet-core-contract-client/src/interfaces/validity_core_contract.rs
@@ -1,76 +1,81 @@
 use async_trait::async_trait;
-use ethers::contract::ContractError;
 use ethers::middleware::Middleware;
-use ethers::prelude::abigen;
-use ethers::types::{TransactionReceipt, I256, U256};
 
 use crate::Error;
 
-abigen!(
-    StarknetValidityContract,
-    r#"[
-        function setProgramHash(uint256 newProgramHash) external notFinalized onlyGovernance
-        function setConfigHash(uint256 newConfigHash) external notFinalized onlyGovernance
-        function setMessageCancellationDelay(uint256 delayInSeconds) external notFinalized onlyGovernance
+use alloy::{
+    network::Ethereum,
+    primitives::{U256, I256},
+    providers::Provider,
+    rpc::types::eth::TransactionReceipt,
+    sol,
+    sol_types::ContractError,
+};
 
-        function programHash() public view returns (uint256)
-        function configHash() public view returns (uint256)
+sol!(
+    #[allow(missing_docs)]
+    interface StarknetValidityContract {
+        function setProgramHash(uint256 newProgramHash) external notFinalized onlyGovernance;
+        function setConfigHash(uint256 newConfigHash) external notFinalized onlyGovernance;
+        function setMessageCancellationDelay(uint256 delayInSeconds) external notFinalized onlyGovernance;
 
-        function identify() external pure override returns (string memory)
-        function stateRoot() external view returns (uint256)
-        function stateBlockNumber() external view returns (int256)
-        function stateBlockHash() external view returns (uint256)
+        function programHash() public view returns (uint256);
+        function configHash() public view returns (uint256);
 
-        function updateState(uint256[] calldata programOutput, uint256 onchainDataHash, uint256 onchainDataSize) external onlyOperator
+        function identify() external pure override returns (string memory);
+        function stateRoot() external view returns (uint256);
+        function stateBlockNumber() external view returns (int256);
+        function stateBlockHash() external view returns (uint256);
 
-    ]"#,
+        function updateState(uint256[] calldata programOutput, uint256 onchainDataHash, uint256 onchainDataSize) external onlyOperator;
+    }
 );
 
 #[async_trait]
-pub trait StarknetValidityContractTrait<M: Middleware> {
+pub trait StarknetValidityContractTrait<P: Provider<Ethereum>> {
     async fn set_program_hash(
         &self,
         new_program_hash: U256,
-    ) -> Result<Option<TransactionReceipt>, Error<M>>;
+    ) -> Result<Option<TransactionReceipt>, Error<P>>;
     async fn set_config_hash(
         &self,
         new_config_hash: U256,
-    ) -> Result<Option<TransactionReceipt>, Error<M>>;
+    ) -> Result<Option<TransactionReceipt>, Error<P>>;
     async fn set_message_cancellation_delay(
         &self,
         delay_in_seconds: U256,
-    ) -> Result<Option<TransactionReceipt>, Error<M>>;
+    ) -> Result<Option<TransactionReceipt>, Error<P>>;
 
-    async fn program_hash(&self) -> Result<U256, Error<M>>;
-    async fn config_hash(&self) -> Result<U256, Error<M>>;
+    async fn program_hash(&self) -> Result<U256, Error<P>>;
+    async fn config_hash(&self) -> Result<U256, Error<P>>;
 
-    async fn identify(&self) -> Result<String, Error<M>>;
-    async fn state_root(&self) -> Result<U256, Error<M>>;
-    async fn state_block_number(&self) -> Result<I256, Error<M>>;
-    async fn state_block_hash(&self) -> Result<U256, Error<M>>;
+    async fn identify(&self) -> Result<String, Error<P>>;
+    async fn state_root(&self) -> Result<U256, Error<P>>;
+    async fn state_block_number(&self) -> Result<I256, Error<P>>;
+    async fn state_block_hash(&self) -> Result<U256, Error<P>>;
     /// Update the L1 state
     async fn update_state(
         &self,
         program_output: Vec<U256>,
         onchain_data_hash: U256,
         onchain_data_size: U256,
-    ) -> Result<Option<TransactionReceipt>, Error<M>>;
+    ) -> Result<Option<TransactionReceipt>, Error<P>>;
 }
 
 #[async_trait]
-impl<T, M: Middleware> StarknetValidityContractTrait<M> for T
+impl<T, P: Provider<Ethereum>> StarknetValidityContractTrait<P> for T
 where
-    T: AsRef<StarknetValidityContract<M>> + Send + Sync,
+    T: AsRef<StarknetValidityContract<P>> + Send + Sync,
 {
     async fn set_program_hash(
         &self,
         new_program_hash: U256,
-    ) -> Result<Option<TransactionReceipt>, Error<M>> {
+    ) -> Result<Option<TransactionReceipt>, Error<P>> {
         self.as_ref()
             .set_program_hash(new_program_hash)
             .send()
             .await
-            .map_err(Into::<ContractError<M>>::into)?
+            .map_err(Into::<ContractError<P>>::into)?
             .await
             .map_err(Into::into)
     }
@@ -78,12 +83,12 @@ where
     async fn set_config_hash(
         &self,
         new_config_hash: U256,
-    ) -> Result<Option<TransactionReceipt>, Error<M>> {
+    ) -> Result<Option<TransactionReceipt>, Error<P>> {
         self.as_ref()
             .set_config_hash(new_config_hash)
             .send()
             .await
-            .map_err(Into::<ContractError<M>>::into)?
+            .map_err(Into::<ContractError<P>>::into)?
             .await
             .map_err(Into::into)
     }
@@ -91,17 +96,17 @@ where
     async fn set_message_cancellation_delay(
         &self,
         delay_in_seconds: U256,
-    ) -> Result<Option<TransactionReceipt>, Error<M>> {
+    ) -> Result<Option<TransactionReceipt>, Error<P>> {
         self.as_ref()
             .set_message_cancellation_delay(delay_in_seconds)
             .send()
             .await
-            .map_err(Into::<ContractError<M>>::into)?
+            .map_err(Into::<ContractError<P>>::into)?
             .await
             .map_err(Into::into)
     }
 
-    async fn program_hash(&self) -> Result<U256, Error<M>> {
+    async fn program_hash(&self) -> Result<U256, Error<P>> {
         self.as_ref()
             .program_hash()
             .call()
@@ -109,19 +114,19 @@ where
             .map_err(Into::into)
     }
 
-    async fn config_hash(&self) -> Result<U256, Error<M>> {
+    async fn config_hash(&self) -> Result<U256, Error<P>> {
         self.as_ref().config_hash().call().await.map_err(Into::into)
     }
 
-    async fn identify(&self) -> Result<String, Error<M>> {
+    async fn identify(&self) -> Result<String, Error<P>> {
         self.as_ref().identify().call().await.map_err(Into::into)
     }
 
-    async fn state_root(&self) -> Result<U256, Error<M>> {
+    async fn state_root(&self) -> Result<U256, Error<P>> {
         self.as_ref().state_root().call().await.map_err(Into::into)
     }
 
-    async fn state_block_number(&self) -> Result<I256, Error<M>> {
+    async fn state_block_number(&self) -> Result<I256, Error<P>> {
         self.as_ref()
             .state_block_number()
             .call()
@@ -129,7 +134,7 @@ where
             .map_err(Into::into)
     }
 
-    async fn state_block_hash(&self) -> Result<U256, Error<M>> {
+    async fn state_block_hash(&self) -> Result<U256, Error<P>> {
         self.as_ref()
             .state_block_hash()
             .call()
@@ -142,12 +147,12 @@ where
         program_output: Vec<U256>,
         onchain_data_hash: U256,
         onchain_data_size: U256,
-    ) -> Result<Option<TransactionReceipt>, Error<M>> {
+    ) -> Result<Option<TransactionReceipt>, Error<P>> {
         self.as_ref()
             .update_state(program_output, onchain_data_hash, onchain_data_size)
             .send()
             .await
-            .map_err(Into::<ContractError<M>>::into)?
+            .map_err(Into::<ContractError<P>>::into)?
             .await
             .map_err(Into::into)
     }
