ba632bf293c62f3353667e8fdeb433c98351dbcf
Arun Jangra <ocdbytes@Aruns-MacBook-Pro.local>
2024-06-27 12:31:33 +0530

    feat : added soveriegn and validity contract and functions

--- crates/l2/starknet-core-contract-client/src/clients/validity.rs
+++ crates/l2/starknet-core-contract-client/src/clients/validity.rs
@@ -2,9 +2,11 @@ use std::sync::Arc;
 
 use ethers::abi::Address;
 
-use crate::interfaces::{Operator, StarknetMessaging, StarknetValidityContract};
+use crate::interfaces::{
+    GovernedFinalizable, Operator, StarknetGovernance, StarknetMessaging, StarknetValidityContract,
+};
 use starknet_proxy_client::proxy_support::ProxySupport;
-use utils::LocalWalletSignerMiddleware;
+use utils::{LocalWalletSignerMiddleware, StarknetContractClient};
 
 /// Client to interact with a Starknet core contract running in `Validity` mode
 pub struct StarknetValidityContractClient {
@@ -12,15 +14,25 @@ pub struct StarknetValidityContractClient {
     messaging: StarknetMessaging<LocalWalletSignerMiddleware>,
     operator: Operator<LocalWalletSignerMiddleware>,
     proxy_support: ProxySupport<LocalWalletSignerMiddleware>,
+    governance: StarknetGovernance<LocalWalletSignerMiddleware>,
+    governed_finalizable: GovernedFinalizable<LocalWalletSignerMiddleware>,
+    core_contract_implementation: ethers::addressbook::Address,
 }
 
 impl StarknetValidityContractClient {
-    pub fn new(address: Address, client: Arc<LocalWalletSignerMiddleware>) -> Self {
+    pub fn new(
+        address: Address,
+        client: Arc<LocalWalletSignerMiddleware>,
+        implementation_address: Address,
+    ) -> Self {
         Self {
             core_contract: StarknetValidityContract::new(address, client.clone()),
             messaging: StarknetMessaging::new(address, client.clone()),
             operator: Operator::new(address, client.clone()),
-            proxy_support: ProxySupport::new(address, client),
+            proxy_support: ProxySupport::new(address, client.clone()),
+            governance: StarknetGovernance::new(address, client.clone()),
+            governed_finalizable: GovernedFinalizable::new(address, client.clone()),
+            core_contract_implementation: implementation_address,
         }
     }
 }
@@ -45,5 +57,27 @@ impl AsRef<ProxySupport<LocalWalletSignerMiddleware>> for StarknetValidityContra
 impl AsRef<Operator<LocalWalletSignerMiddleware>> for StarknetValidityContractClient {
     fn as_ref(&self) -> &Operator<LocalWalletSignerMiddleware> {
         &self.operator
+    }
+}
+impl AsRef<StarknetGovernance<LocalWalletSignerMiddleware>> for StarknetValidityContractClient {
+    fn as_ref(&self) -> &StarknetGovernance<LocalWalletSignerMiddleware> {
+        &self.governance
+    }
+}
+impl AsRef<GovernedFinalizable<LocalWalletSignerMiddleware>> for StarknetValidityContractClient {
+    fn as_ref(&self) -> &GovernedFinalizable<LocalWalletSignerMiddleware> {
+        &self.governed_finalizable
+    }
+}
+
+impl StarknetContractClient for StarknetValidityContractClient {
+    fn address(&self) -> ethers::addressbook::Address {
+        self.core_contract.address()
+    }
+    fn implementation_address(&self) -> ethers::addressbook::Address {
+        self.core_contract_implementation
+    }
+    fn client(&self) -> Arc<LocalWalletSignerMiddleware> {
+        self.core_contract.client()
     }
 }
