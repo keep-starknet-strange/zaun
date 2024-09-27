36dc1fc5b2affaee69c669aac97a5062622b43b5
byteZorvin <imehul24@gmail.com>
2024-09-27 16:58:10 +0530

    fix max fee and update type

--- crates/l3/appchain-core-contract-client/src/interfaces/core_contract.rs
+++ crates/l3/appchain-core-contract-client/src/interfaces/core_contract.rs
@@ -1,6 +1,7 @@
 use appchain_utils::invoke_contract;
 use appchain_utils::LocalWalletSignerMiddleware;
 use color_eyre::Result;
+use starknet::core::types::U256;
 use starknet_core::types::{Felt, InvokeTransactionResult};
 
 pub struct CoreContract {
@@ -17,12 +18,14 @@ impl CoreContract {
         &self,
         program_output: Vec<Felt>,
         onchain_data_hash: Felt,
-        onchain_data_size: Felt,
+        onchain_data_size: U256,
     ) -> Result<InvokeTransactionResult> {
         let mut calldata = Vec::with_capacity(program_output.len() + 2);
+        calldata.push(program_output.len().into());
         calldata.extend(program_output);
         calldata.push(onchain_data_hash);
-        calldata.push(onchain_data_size);
+        calldata.push(onchain_data_size.low().into());
+        calldata.push(onchain_data_size.high().into());
 
         invoke_contract(&self.signer, self.address, "update_state", calldata).await
     }
