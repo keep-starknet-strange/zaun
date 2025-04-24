fa8a6428d97617e4ab27f9dcb660cd86bbd59321
Apoorv Sadana <95699312+apoorvsadana@users.noreply.github.com>
2025-04-24 13:55:21 +0530

    add version to rust toolchain (#33)
    * add version to rust toolchain
    
    * fix clippy

--- crates/l3/appchain-core-contract-client/src/lib.rs
+++ crates/l3/appchain-core-contract-client/src/lib.rs
@@ -9,7 +9,7 @@ use std::path::Path;
 
 // TODO: check for proxy contract implementation
 
-pub async fn deploy_starknet_core_contract<'a>(
+pub async fn deploy_starknet_core_contract(
     signer: LocalWalletSignerMiddleware,
     path_to_sierra: &Path,
     path_to_casm: &Path,
